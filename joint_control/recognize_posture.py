e9d6273d79e87cfeaf31e7828ccd44e325f06618
SchoolIsak <isak.schulman25@gmail.com>
2025-11-13 14:05:06 +0400

    Implement posture classification in recognize_posture.py
    Added posture classifier loading and feature extraction for posture recognition.

--- joint_control/recognize_posture.py
+++ joint_control/recognize_posture.py
@@ -12,6 +12,8 @@
 
 from angle_interpolation import AngleInterpolationAgent
 from keyframes import hello
+import pickle
+import os
 
 
 class PostureRecognitionAgent(AngleInterpolationAgent):
@@ -22,17 +24,38 @@ class PostureRecognitionAgent(AngleInterpolationAgent):
                  sync_mode=True):
         super(PostureRecognitionAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
         self.posture = 'unknown'
-        self.posture_classifier = None  # LOAD YOUR CLASSIFIER
+        
+        ROBOT_POSE_CLF = os.path.join('joint_control', 'robot_pose.pkl')
+        self.posture_classifier = pickle.load(open(ROBOT_POSE_CLF, "rb"))  # LOAD YOUR CLASSIFIER
 
     def think(self, perception):
         self.posture = self.recognize_posture(perception)
         return super(PostureRecognitionAgent, self).think(perception)
 
     def recognize_posture(self, perception):
-        posture = 'unknown'
-        # YOUR CODE HERE
+        # Getting all features from perception
+        features = [
+            perception.joint['LHipYawPitch'],
+            perception.joint['LHipRoll'],
+            perception.joint['LHipPitch'],
+            perception.joint['LKneePitch'],
+            perception.joint['RHipYawPitch'],
+            perception.joint['RHipRoll'],
+            perception.joint['RHipPitch'],
+            perception.joint['RKneePitch'],
+            perception.imu[0],
+            perception.imu[1]
+        ]
+        # Reshaping ?
+        features = [features]  # shape (1, 10)
+
+        # Posture index prediction
+        posture = self.posture_classifier.predict(features)[0]
+
+        # print(posture)
 
         return posture
+
 
 if __name__ == '__main__':
     agent = PostureRecognitionAgent()
