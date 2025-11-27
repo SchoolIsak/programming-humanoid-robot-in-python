bb5f61da6f7e21ee1bbbea2011aebd17af419778
SchoolIsak <isak.schulman25@gmail.com>
2025-11-27 15:28:12 +0100

    Update forward_kinematics.py

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -1,3 +1,4 @@
+# NOTE: This file was edited 27.11, since it didn't work prior and this was discovered in the next assignment
 '''In this exercise you need to implement forward kinematics for NAO robot
 
 * Tasks:
@@ -21,10 +22,8 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
-import autograd.numpy as anp
-
-
 from recognize_posture import PostureRecognitionAgent
+import autograd.numpy as anp
 
 
 class ForwardKinematicsAgent(PostureRecognitionAgent):
@@ -34,7 +33,7 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
                  player_id=0,
                  sync_mode=True):
         super(ForwardKinematicsAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
-        self.transforms = {n: anp.eye(4) for n in self.joint_names}
+        self.transforms = {n: anp.eye(4, dtype=anp.float64) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
         self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
@@ -49,33 +48,35 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         return super(ForwardKinematicsAgent, self).think(perception)
 
     def local_trans(self, joint_name, angle):
-        T = anp.eye(4)
+        T = anp.eye(4, dtype=anp.float64)
 
-       
         # First we consider the rotation axis for every joint
         if joint_name in ["HeadYaw", "LElbowYaw", "RElbowYaw"]:
             # Z axis
-            R = anp.array([[anp.cos(angle), -anp.sin(angle), 0],
-                           [anp.sin(angle),  anp.cos(angle), 0],
-                           [0, 0, 1]]) 
+            R = anp.array([[anp.cos(angle), -anp.sin(angle), 0.0],
+                           [anp.sin(angle),  anp.cos(angle), 0.0],
+                           [0.0, 0.0, 1.0]], dtype=anp.float64)
 
+        
         elif joint_name in ["HeadPitch", "LShoulderPitch", "RShoulderPitch",
                             "LHipPitch", "RHipPitch",
                             "LKneePitch", "RKneePitch",
                             "LAnklePitch", "RAnklePitch"]:
             # Y axis
-            R = anp.array([[anp.cos(angle), 0, anp.sin(angle)],
-                           [0, 1, 0],
-                           [-anp.sin(angle), 0, anp.cos(angle)]]) 
+            R = anp.array([[anp.cos(angle), 0.0, anp.sin(angle)],
+                           [0.0, 1.0, 0.0],
+                           [-anp.sin(angle), 0.0, anp.cos(angle)]], dtype=anp.float64)
+
 
         elif joint_name in ["LShoulderRoll", "RShoulderRoll",
                             "LElbowRoll", "RElbowRoll",
                             "LHipRoll", "RHipRoll",
                             "LAnkleRoll", "RAnkleRoll"]:
             # X axis
-            R = anp.array([[1, 0, 0],
-                           [0, anp.cos(angle), -anp.sin(angle)],
-                           [0, anp.sin(angle),  anp.cos(angle)]])  # 
+            R = anp.array([[1.0, 0.0, 0.0],
+                           [0.0, anp.cos(angle), -anp.sin(angle)],
+                           [0.0, anp.sin(angle),  anp.cos(angle)]], dtype=anp.float64)
+
 
         # Special: HipYawPitch has a 45° rotated axis
         elif joint_name in ["LHipYawPitch", "RHipYawPitch"]:
@@ -84,15 +85,15 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
             axis = 1.0 / anp.sqrt(2.0)
             # Rotation around axis (x+z)/√2
             R = anp.array([
-                [axis*axis*(1-c)+c, 0, axis*axis*(1-c)-axis*s],
-                [0, 1, 0],
-                [axis*axis*(1-c)+axis*s, 0, axis*axis*(1-c)+c]
-            ])
+                [axis*axis*(1.0-c)+c, 0.0, axis*axis*(1.0-c)-axis*s],
+                [0.0, 1.0, 0.0],
+                [axis*axis*(1.0-c)+axis*s, 0.0, axis*axis*(1.0-c)+c]
+            ], dtype=anp.float64)
         else:
             # Else we set R to be eye so no error
-            R = anp.eye(3)
+            R = anp.eye(3, dtype=anp.float64)
 
-       # Joint translations
+        # Joint translations
         dx = dy = dz = 0.0
 
         # We get the needed values directly from the documentation
@@ -130,21 +131,43 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
 
         if joint_name in translations:
             dx, dy, dz = translations[joint_name]
+            dx = float(dx); dy = float(dy); dz = float(dz)
 
-       # Construct output transformation matrix
-        T = T.copy()  # important for autograd
-        T[0:3, 0:3] = R
-        T[0:3, 3] = anp.array([dx, dy, dz])
+        # Construct output transformation matrix
+       
+        t_col = anp.reshape(anp.array([dx, dy, dz], dtype=anp.float64), (3, 1))
+        top = anp.hstack((R, t_col))          # shape (3,4)
+
+        # Bottom row [0 0 0 1]
+        bottom = anp.array([[0.0, 0.0, 0.0, 1.0]], dtype=anp.float64)
+
+        # Final T
+        T = anp.vstack((top, bottom))        
 
         return T
 
-    def forward_kinematics(self, joints):
+    def forward_kinematics(self, joints, effector=None):
         for chain, chain_joints in self.chains.items():
-            T = anp.eye(4)
+            T = anp.eye(4, dtype=anp.float64)
             for j in chain_joints:
                 angle = joints[j]
                 T = T @ self.local_trans(j, angle)
                 self.transforms[j] = T
+
+        # If caller asks for a particular effector, return its 4x4 transform (autograd-friendly)
+        if effector is not None:
+            # If user passed a chain name (like "LLeg"), return transform of its last joint
+            if effector in self.chains:
+                last_joint = self.chains[effector][-1]
+                return self.transforms[last_joint]
+            # If user passed a joint name directly, return it
+            if effector in self.transforms:
+                return self.transforms[effector]
+            # Helpful error if user passed something unknown
+            raise KeyError(f"Unknown effector '{effector}'. valid chain names: {list(self.chains.keys())}, "
+                        f"valid joint names: {list(self.transforms.keys())}")
+
+        return self.transforms
 
     
 
